77fa5267a890c938d01f9673a0cc999707ed6ed9
Wellington Maciel Guimarães <112827685+IIWARLORDII@users.noreply.github.com>
2024-11-27 20:02:24 -0300

    Adicionado uso de .env

--- AV2/app.js
+++ AV2/app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
-const sequelize = new Sequelize('5sbd_av2', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 const Aeroporto = sequelize.define('Aeroporto', {
   codigo: {
